fd47b80372a843a067a6d45ddf732db60ee68133
Ava <149529291+Ava-but@users.noreply.github.com>
2025-02-26 18:16:31 +0200

    html

--- script.js
+++ script.js
@@ -1,29 +1,6 @@
-function searchFunction() {
-    let input = document.getElementById("searchInput").value.toLowerCase();
-    let items = document.querySelectorAll("");
+const btn = document.querySelector("button.mobile-menu-button");
+const menu = document.querySelector(".mobile-menu");
 
-    items.forEach(item => {
-        let text = item.textContent.toLowerCase();
-        item.classList.toggle("hide", !text.includes(input));
-    });
-}
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    loop: true,
-    slidesPerView:3,
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
